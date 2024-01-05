fba7cc3324cf2763af8820b49e9d8a35d221fa55
houdamzn <133459033+houdamzn@users.noreply.github.com>
2024-01-05 14:43:21 +0100

    Update XO game.py

--- XO game.py
+++ XO game.py
@@ -1,126 +1,91 @@
-from tkinter import * 
-from glob import glob
+from tkinter import *
 from random import choice
-import random
 
-#give turn for the auther gamer
 def next_turn(row, col):
     global player
     if game_btns[row][col]['text'] == "" and check_winner() == False:
-        # Put player's symbol
         game_btns[row][col]['text'] = player
 
         if check_winner() == False:
-            # Switch player
             player = players[1] if player == players[0] else players[0]
             label.config(text=(player + " turn"))
 
             if player == "O":
-                # Computer's move
                 computer_move()
 
         elif check_winner() == True:
-         label.config(text=(player + " wins!"))
+            label.config(text=(player + " wins!"))
         elif check_winner() == 'tie':
-         label.config(text=("Tie, No Winner!"))
+            label.config(text=("Tie, No Winner!"))
 
-
-
-
-# Computer's move (AI)
-def computer_move():
+def A_algorithm():
     empty_cells = [(r, c) for r in range(3) for c in range(3) if game_btns[r][c]['text'] == ""]
+    
     if empty_cells:
-        row, col = choice(empty_cells)
+        return choice(empty_cells)
+    else:
+        return None
+
+def computer_move():
+    move = A_algorithm()
+    
+    if move:
+        row, col = move
         game_btns[row][col].invoke()
 
-
-#if there is winner (true)
-def check_winner(): 
-     # check all 3 horizontal conditions
+def check_winner():
     for row in range(3):
         if game_btns[row][0]['text'] == game_btns[row][1]['text'] == game_btns[row][2]['text'] != "":
-            game_btns[row][0].config(bg="cyan")
-            game_btns[row][1].config(bg="cyan")
-            game_btns[row][2].config(bg="cyan")
+            highlight_winner_cells([(row, 0), (row, 1), (row, 2)])
             return True
 
-    # check all 3 vertical conditions
     for col in range(3):
         if game_btns[0][col]['text'] == game_btns[1][col]['text'] == game_btns[2][col]['text'] != "":
-            game_btns[0][col].config(bg="cyan")
-            game_btns[1][col].config(bg="cyan")
-            game_btns[2][col].config(bg="cyan")
+            highlight_winner_cells([(0, col), (1, col), (2, col)])
             return True
 
-    # check diagonals conditions
     if game_btns[0][0]['text'] == game_btns[1][1]['text'] == game_btns[2][2]['text'] != "":
-        game_btns[0][0].config(bg="cyan")
-        game_btns[1][1].config(bg="cyan")
-        game_btns[2][2].config(bg="cyan")
+        highlight_winner_cells([(0, 0), (1, 1), (2, 2)])
         return True
     elif game_btns[0][2]['text'] == game_btns[1][1]['text'] == game_btns[2][0]['text'] != "":
-        game_btns[0][2].config(bg="cyan")
-        game_btns[1][1].config(bg="cyan")
-        game_btns[2][0].config(bg="cyan")
+        highlight_winner_cells([(0, 2), (1, 1), (2, 0)])
         return True
 
-    # if there are no empty spaces left
     if check_empty_spaces() == False:
         for row in range(3):
             for col in range(3):
                 game_btns[row][col].config(bg='red')
-
         return 'tie'
-
     else:
         return False
 
-
-
-
-#if all of them empty game end 
 def check_empty_spaces():
-     spaces = 9
+    return any(game_btns[row][col]['text'] == "" for row in range(3) for col in range(3))
 
-     for row in range(3):
-        for col in range(3):
-            if game_btns[row][col]['text'] != "":
-                spaces -= 1
+def highlight_winner_cells(cells):
+    for cell in cells:
+        game_btns[cell[0]][cell[1]].config(bg="cyan")
 
-     if spaces == 0:
-        return False
-     else:
-        return True
-
-
-#for starting new game
 def start_new_game():
     global player
-    player = "O"  # User always starts with 'O'
+    player = "O"
     label.config(text=(player + " turn"))
 
     for row in range(3):
         for col in range(3):
             game_btns[row][col].config(text="", bg="#F0F0F0")
 
-    # If the computer is the first player, make its move
     if player == "X":
         computer_move()
-
-    elif player =="O":
+    elif player == "O":
         computer_move()
-        
+
 window = Tk()
 window.title("Tic_Tac_Toe")
 
-players = ["X", "O"]  # our players
+players = ["X", "O"]
 player = choice(players)
-game_btns = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
+game_btns = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 label = Label(text=(player, "turn"), font=('consolas', 40))
 label.pack(side="top")
@@ -136,6 +101,5 @@ for row in range(3):
         game_btns[row][col] = Button(btns_frame, text="", font=('consolas', 50), width=4, height=1,
                                      command=lambda row=row, col=col: next_turn(row, col))
         game_btns[row][col].grid(row=row, column=col)
-
 
 window.mainloop()
